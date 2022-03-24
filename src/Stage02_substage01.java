4ee446fce2f88e1b2776142e1259516d630a4ece
madsteel63 <silverlightoffallingstar@gmail.com>
2022-03-24 09:10:04 +0400

    Initial commit

---
+++ src/Stage02_substage01.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class Stage02_substage01 {
+    public static void main(String[] args) {
+        System.out.println("Вычислить ряд Фибоначчи.Количество чисел в ряду Фибоначчи ввести с консоли:\n=====================");
+
+
+    }
+}
